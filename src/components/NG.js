a5900c903a2f106b57c1687b5f24b34c361e5a6e
nikhilgupta12 <nikhil.gupta@sensen.ai>
2024-01-04 20:37:26 +0530

    first commit

---
+++ src/components/NG.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function NG() {
+  return (
+    <>
+
+        <div className='container mt-5 ' >
+            <div className='row light-text profile-content '>
+                <div className='img-container'>
+                    <img className='profile-image' src='/portfolio/public/IMG_0705 (copy).JPG' alt=''/>
+                </div>
+                <div className='about-text mt-5'>
+
+                    <h1 className='text-center mt-5'>
+                        Hey, I'm Nikhil Gupta
+                    </h1>
+                    <h4>
+                    I am a software engineer with 3 years of experience in developing and maintaining 
+                    software applications. I have hands-on experience in full stack web
+                    development and software engineering. My greatest strength is my ability to
+                    analyze complex problems and design efficient solutions. I possess excellent
+                    communication skills that help me collaborate effectively with other teams.
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </>
+    
+  )
+}
