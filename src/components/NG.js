8ee333244090ed2c0865a3498f5676be77a1c955
nikhilgupta12 <nikhil.gupta@sensen.ai>
2024-01-05 21:17:35 +0530

    added my photo

--- src/components/NG.js
+++ src/components/NG.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import myImg from '../media/my.JPG'
 
 export default function NG() {
   return (
@@ -6,22 +7,22 @@ export default function NG() {
 
         <div className='container mt-5 ' >
             <div className='row text-white profile-content '>
-                <div className='img-container'>
-                    <img className='profile-image' src='/portfolio/public/IMG_0705 (copy).JPG' alt=''/>
+                <div className='d-flex img-container col-12'>
+                    <img className='profile-image col-6' src={myImg} alt=''/>
+                    <div className='about-text col-6 ms-5 mt-5'>
+                        <h1 className='text-center'>
+                            Hey, I'm Nikhil Gupta
+                        </h1>
+                        <h5 className='text-center mt-5'>
+                        I am a software engineer with 3 years of experience in developing and maintaining 
+                        software applications. I have hands-on experience in full stack web
+                        development and software engineering. My greatest strength is my ability to
+                        analyze complex problems and design efficient solutions. I possess excellent
+                        communication skills that help me collaborate effectively with other teams.
+                        </h5>
+                    </div>
                 </div>
-                <div className='about-text mt-5'>
-
-                    <h1 className='text-center mt-5'>
-                        Hey, I'm Nikhil Gupta
-                    </h1>
-                    <h4>
-                    I am a software engineer with 3 years of experience in developing and maintaining 
-                    software applications. I have hands-on experience in full stack web
-                    development and software engineering. My greatest strength is my ability to
-                    analyze complex problems and design efficient solutions. I possess excellent
-                    communication skills that help me collaborate effectively with other teams.
-                    </h4>
-                </div>
+                
             </div>
         </div>
     </>
