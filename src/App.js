90a249d4901f896b27c64649925a7d9820613ec4
nikhilgupta12 <nikhil.gupta@sensen.ai>
2024-01-05 18:31:08 +0530

    added resume component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import NG from './components/NG';
 import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from 'react-router-dom';
+import Resume from './components/Resume';
+
 
 function App() {
   return (
     <>
+    <Router>
+
       <Navbar title="NG" />
-      <NG/>
+      <Routes>
+        <Route path="/" element={<NG />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/about" element={<NG />} />
+
+      </Routes>
+    </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import ngsu from '../media/ngsu.png';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid ">
         {/* <a className="navbar-brand text-white" href="/">{props.title}</a> */}
-        <a className="navbar-brand text-white" href="/"><img src={ngsu} width={100}/></a>
+        <Link className="navbar-brand text-white" to="/"><img src={ngsu} width={100} alt=''/></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon text-white"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item ms-5">
-                <a className="nav-link active text-white" aria-current="page" href="/">Resume</a>
+                <Link className="nav-link active text-white" aria-current="page" to="/resume">Resume</Link>
             </li>
             <li className="nav-item ms-5">
-                <a className="nav-link text-white" href="/">About</a>
+                <Link className="nav-link text-white" to="/about">About</Link>
             </li>
             
             
